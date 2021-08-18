7380a0fa5abcd9af0e8f4dd14958f0fe6a2f7d17
BatistaCR <alexbatistacr@gmail.com>
2021-08-18 01:45:14 -0600

    FORMULARIO DETALLES

--- detalle.php
+++ detalle.php
@@ -38,6 +38,15 @@ $exe = mysqli_query($conexion,$consul);
 while($t = mysqli_fetch_array($exe)){ ?>
 
 <style type="text/css">
+
+input[type=number]::-webkit-inner-spin-button,
+  input[type=number]::-webkit-outer-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+      }
+  input[type=number] { -moz-appearance:textfield; }
+
+
 .caja{
   
 overflow: hidden;
@@ -60,10 +69,24 @@ overflow: hidden;
   border-radius: 0.5rem;
   border: solid 2px blue;
   width: auto;
-  height: 2rem;
+  height: 3rem;
   align-items: center;
   justify-content: center;
   background-color: white;
+  
+  overflow: hidden;
+}
+
+.input{
+text-align: center;
+width: 2rem;
+}
+
+.btn-carrito{
+  background-color: red;
+  color: white;
+  border-radius: 0.5rem;
+  
 }
 </style>
 
@@ -75,13 +98,42 @@ overflow: hidden;
 </nav>
 <!--BREADCRUM-->
 
+
+<form action="#" method="POST">
 <div class="container pf-02 ja-center">
   <div class="row">
 <div class="col-auto caja">
           <img src="img/productos/<?php echo $t['img_producto']; ?>" alt="...">
 </div>
 
-<div class="col px-05">
+<script>
+  
+  function mas() {
+      document.getElementById("input").value++;
+  }
+  
+  function menos() {
+      var valor = document.getElementById("input").value;
+      
+  
+      if (valor == 1) {
+    }else if(valor < 1){
+      document.getElementById("input").value++;
+    }
+    else{
+        document.getElementById("input").value--;
+    }
+      
+  }
+
+  function pierdeFoco(e){
+    var valor = e.value.replace(/^0*/, '');
+    e.value = valor;
+  }
+  </script>
+
+
+<div class="col px-05 bc-yellow">
 <h1 class="ts-09" ><b><?php echo $t['nombre_prod_inv']; ?></b></h1>
 <?php 
               if($t['precio_unidad'] == "" AND $t['precio_caja'] != "") {  ?>
@@ -94,14 +146,21 @@ overflow: hidden;
             echo "<p class='fs-10'><b>VER DETALLES</b></p>";
             }
             ?>
-            <div class="row ja-center bc-red ta-center fd-row">
-            <p class="fs-05 mx-05">cantidad</p> 
-            <div class="fd-row contador">
-            <button type="button" class="btn-box" value="-">-</button>
-            <input type="number" name="" id="">
-            <button type="button" class="btn-box" value="+">+</button>
-            </div>
+            <div class="bc-red row ja-center ta-center fd-row">
+              <div class="col-6">
+            <p class="fs-05">cantidad</p> 
+            <div class="fd-row contador bc-blue">
+            <button type="button" class="btn-box" onclick="menos()">-</button>
+            <input class="input" type="number" name="" id="input" value="1" min="1" oninput="this.value = Math.abs(this.value)" 
+            onKeyUp="pierdeFoco(this)" required>
+            <button type="button" class="btn-box" onclick="mas()">+</button>
           </div>
+            </div>
+            <div class="col-6">
+            <button class="btn-carrito pf-03" type="submit"><b>IR AL CARRITO </b> </button>
+          </div>
+          </div>
+          
 </div>
 </div>
   
@@ -111,7 +170,7 @@ overflow: hidden;
     ?>
   </div><br>
 
-
+</form>
 
 
   <!-- Footer -->
