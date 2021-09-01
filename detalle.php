7aad2c9f1e9c52391e5ee305c8ef6c3bbcff7e8c
randallST <40444600+randallST@users.noreply.github.com>
2021-09-01 00:00:43 -0600

    renuncia
    .

--- detalle.php
+++ detalle.php
@@ -11,13 +11,18 @@ header("Cache-Control: no-cache, must-revalidate");
      $id_cliente = $u['id_user_general'];
     }
       }else{   
-      header("location:../../PHARMATICAS/PHARMATICAS/sesion.php");
+     // header("location:../../PHARMATICA/sesion.php");
       }
 ?>
  
 <!DOCTYPE html>
 <html lang="es">
 <!-- Head -->
+
+<!-- desactiva el clik der e inspeccionar -->
+<body oncontextmenu="return false" >
+
+
 <?php include('vistas/head.php') ?>
 
 <body>
@@ -40,12 +45,14 @@ header("Cache-Control: no-cache, must-revalidate");
 
     while($t = mysqli_fetch_array($exe)){ 
       /*datos princiapales de la consulta*/
+      $nombre1 = $t['nombre_prod_inv'];
       $cantidad_unidad1 = $t['cantidad_prod_unidades'];
       $cantidad_caja1 = $t['cantidad_prod_cajas'];
       $precio_unidad = $t['precio_unidad'];
       $precio_caja = $t['precio_caja'];
 ?>
 
+<!--<a href="https://web.whatsapp.com/send?phone=50687472886&text=https://www.youtube.com/watch?v=<?php // echo $nombre1; ?>" target="_blank">WHATSAPP</a><br>-->
 
 <!--BREADCRUM-->
 <nav aria-label="breadcrumb" class="bc-black py-01">
@@ -60,9 +67,9 @@ header("Cache-Control: no-cache, must-revalidate");
   <div class="container">
     <div class="row pf-03 ja-center my-05">
 
-      <!-- input oculto de envio de ID del product -->                       
-      <input type="hidden" name="id_prod_detalle" 
-        value="<?php echo $t['id_prod_inv']; ?>">
+      <!-- input oculto de envio de CODIGO del product -->
+      <input type="hidden" name="cod_prod_detalle" 
+        value="<?php echo $t['codigo_producto']; ?>">
 
       <!-- input oculto que envia el ID del user -->
       <input type="hidden" name="id_cliente"
@@ -70,31 +77,44 @@ header("Cache-Control: no-cache, must-revalidate");
 
 
 <script type="text/javascript">
-  
-  function mas() {
-      document.getElementById("input").value++;
-  }
-  
-  function menos() {
-      var valor = document.getElementById("input").value;
-      
-  
-      if (valor == 1) {
-    }else if(valor < 1){
-      document.getElementById("input").value++;
-    }
-    else{
-        document.getElementById("input").value--;
-    }
-      
-  }
-
-  function pierdeFoco(e){
-    var valor = e.value.replace(/^0*/, '');
-    e.value = valor;
-  }
 
 function changeFunc(){
+
+
+if(document.getElementById('selectBox').value == "UNIDAD_M") {
+
+    document.getElementById('caja_pri').style.display = "none";
+    document.getElementById('caja_secun').style.display = "";
+
+
+    function mas_uni() {
+    
+     let valor_actual = document.getElementById("input_caja2").value;
+     var cantidad_general = "<?php echo $precio_unidad; ?>";
+     valor_actual++;
+  
+      if(valor_actual<=cantidad_general){
+         document.getElementById("input_caja2").value=valor_actual++;
+      }
+  }
+  
+  function menos_uni() {
+      var valor = document.getElementById("input_caja2").value;  
+      if (valor == 1) {
+    }else if(valor < 1){
+      document.getElementById("input_caja2").value++;
+    }
+    else{
+        document.getElementById("input_caja2").value--;
+    }
+      
+  }
+  
+}
+
+
+
+
   
 //  document.write(nombre);
 var caja = document.getElementById("caja");
@@ -104,12 +124,12 @@ var tipo_compra = selectBox.options[selectBox.selectedIndex].value;
 //var caja = selectBox.options[selectBox.selectedIndex].value="UNIDAD";
    // alert(selectedValue);
 
-if (tipo_compra === "CAJA") {
+if (tipo_compra === "CAJA_M") {
     unidad.style.display = "none";
     caja.style.display = "";
     var cantidad_general1 = "<?php echo $cantidad_caja1; ?>"
     //document.write(cantidad_general1);
-}else if (tipo_compra === "UNIDAD") {
+}else if (tipo_compra === "UNIDAD_M") {
     unidad.style.display = "";
     caja.style.display = "none";
     var cantidad_general1 = "<?php echo $cantidad_unidad1; ?>"
@@ -134,11 +154,12 @@ if (tipo_compra === "CAJA") {
 <?php 
   /*solo muestra precio unidad*/
   if($t['precio_unidad'] != "" AND $t['precio_caja'] === ""){ 
-     $cantidad_general = $t['cantidad_prod_unidades'];
+      $cantidad_general = $t['cantidad_prod_unidades'];
   ?>
     <div id="unidad" style="display:;"> 
       <p class="fs-03">PRECIO POR UNIDAD</p>
       <p class="fs-05 cr-red">₡<?php echo $t['precio_unidad']; ?>+i.v.a.</p>
+      <p class="fs-05 cr-blue">DISPONIBLE: <?php echo $t['cantidad_prod_unidades']; ?></p>
     </div>
   
 
@@ -150,6 +171,7 @@ if (tipo_compra === "CAJA") {
     <div id="caja" style="display:;"> 
       <p class="fs-03">PRECIO POR CAJA</p>
       <p class="fs-05 cr-red">₡<?php  echo $t['precio_caja'];?>+i.v.a.</p>
+      <p class="fs-05 cr-blue">DISPONIBLE: <?php echo $t['cantidad_prod_cajas'];?></p>
     </div>
 
 <?php  
@@ -161,16 +183,20 @@ if (tipo_compra === "CAJA") {
     <div id="unidad" style="display:;"> 
       <p class="fs-03">PRECIO POR UNIDAD</p>
       <p class="fs-05 cr-red">₡<?php echo $t['precio_unidad']; ?>+i.v.a.</p>
+      <p class="fs-05 cr-blue">DISPONIBLE: 
+        <?php echo $t['cantidad_prod_unidades']; ?></p>
     </div> 
 
     <div id="caja" style="display:none;"> 
       <p class="fs-03">PRECIO POR CAJA</p>
       <p class="fs-05 cr-red">₡<?php  echo $t['precio_caja']; ?>+i.v.a.</p>
+      <p class="fs-05 cr-blue"> DISPONIBLE: <?php 
+      echo $t['cantidad_prod_cajas']; ?></p>
     </div>
 <?php  
   }else{
     /*Si NO tiene los precios establecidos*/
-    echo "<p class='fs-05'>VER DETALLES</p>";
+    echo "<p class='fs-05'>CONSULTAR DETALLES</p>";
   }
 ?>
 
@@ -199,8 +225,8 @@ if (tipo_compra === "CAJA") {
   /*si tiene ambos precios*/
   }elseif($precio_unidad != "" AND $precio_caja != ""){
 ?>
-  <option value="UNIDAD">UNIDAD</option>
-  <option value="CAJA">CAJA</option>
+  <option value="UNIDAD_M" id="uni">UNIDAD</option>
+  <option value="CAJA_M" id="caj">CAJA</option>
 
 <?php  
   }
@@ -208,11 +234,53 @@ if (tipo_compra === "CAJA") {
 </select>
 
 
+
+<script type="text/javascript">
+
+  function mas() {
+    
+     let valor_actual = document.getElementById("input").value;
+     var cantidad_general = "<?php echo $cantidad_general; ?>";
+     valor_actual++;
+  
+      if(valor_actual<=cantidad_general){
+         document.getElementById("input").value=valor_actual++;
+      }
+  }
+  
+  function menos() {
+      var valor = document.getElementById("input").value;  
+      if (valor == 1) {
+    }else if(valor < 1){
+      document.getElementById("input").value++;
+    }
+    else{
+        document.getElementById("input").value--;
+    }
+      
+  }
+
+
+
+
+  window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
+
+</script>
+
+
+
+
 <!-- INICIO input y controles del contador del carrito -->
-<div class="row ">
+<div class="row">
   <div class="col-12 col-md-5 ja-center fd-column">
      <p class="fs-03">Cantidad</p> 
-       <div class="fd-row">
+       <div class="fd-row" id="caja_pri" style="display:;">
             <!-- boton de menos en el contador -->
           <button type="button" class="btn-box-left" 
              onclick="menos()">-
@@ -221,20 +289,35 @@ if (tipo_compra === "CAJA") {
            <!-- input de cantidad --> 
           <input class="input" type="number" name="cantidad_productos" 
             id="input" value="1" oninput="this.value = Math.abs(this.value)"
-             min="1" onKeyUp="pierdeFoco(this)" required max="
-              <?php 
-                 if(isset($cantidad_general)){
-                   echo $cantidad_general;
-                 }else{
-                   
-                 }
-              ?>">
+             min="1"  required>
 
           <!-- input de menos en el contador -->
           <button type="button" class="btn-box-right" 
-              onclick="mas()">+
+              onclick="mas();">+
           </button>
        </div>
+
+       <!-- CAJA 2 -->
+       <div class="fd-row" id="caja_secun" style="display:none;">
+            <!-- boton de menos en el contador -->
+          <button type="button" class="btn-box-left" 
+             onclick="menos_uni()">-
+          </button>
+
+           <!-- input de cantidad --> 
+          <input class="input" type="number" name="cantidad_productos" 
+            id="input_caja2" value="1" oninput="this.value = Math.abs(this.value)" min="1"  required>
+
+          <!-- input de menos en el contador -->
+          <button type="button" class="btn-box-right" 
+              onclick="mas_uni();">+
+          </button>
+       </div>
+
+
+
+      
+
   </div>
 
 
